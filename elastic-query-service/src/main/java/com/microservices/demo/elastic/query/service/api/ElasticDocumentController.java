128aa11b36d16c87e3112a909d86f1e4b01586e4
pvnreddy <pvnreddy459@gmail.com>
2022-04-29 23:12:46 +0530

    completed query service

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -3,6 +3,12 @@ package com.microservices.demo.elastic.query.service.api;
 import com.microservices.demo.elastic.query.service.business.impl.TwitterElasticQueryService;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceRequestModel;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponceModel;
+import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponceModelV2;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/documents")
+@RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
 public class ElasticDocumentController {
     private static final Logger LOG = LoggerFactory.getLogger(ElasticDocumentController.class);
 
@@ -24,6 +30,16 @@ public class ElasticDocumentController {
         this.twitterElasticQueryService = twitterElasticQueryService;
     }
 
+
+    @Operation(summary = "Get All Elastic Documents!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Successful Response", content = {
+                    @Content(mediaType = "application/vnd.api.v1+json",
+                    schema = @Schema(implementation = ElasticQueryServiceResponceModel.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping("/")
     public @ResponseBody
     ResponseEntity<List<ElasticQueryServiceResponceModel>> getAllDocuments(){
@@ -32,6 +48,15 @@ public class ElasticDocumentController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Get Document by id!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Successful Response", content = {
+                    @Content(mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponceModel.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping("/{id}")
     public @ResponseBody
     ResponseEntity<ElasticQueryServiceResponceModel> getDocumentById(@PathVariable @NotEmpty String id){
@@ -39,6 +64,33 @@ public class ElasticDocumentController {
         LOG.info("Eastic search returned document with id",id);
         return ResponseEntity.ok(response);
     }
+
+    @Operation(summary = "Get Document by id!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Successful Response", content = {
+                    @Content(mediaType = "application/vnd.api.v2+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponceModelV2.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @GetMapping(value = "/{id}", produces = "application/vnd.api.v2+json")
+    public @ResponseBody
+    ResponseEntity<ElasticQueryServiceResponceModelV2> getDocumentByIdV2(@PathVariable @NotEmpty String id){
+        ElasticQueryServiceResponceModel response = twitterElasticQueryService.getDocumentById(id);
+        LOG.info("Eastic search returned document with id",id);
+        return ResponseEntity.ok(getmodelV2(response));
+    }
+
+    @Operation(summary = "Get Document by text!")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Successful Response", content = {
+                    @Content(mediaType = "application/vnd.api.v1+json",
+                            schema = @Schema(implementation = ElasticQueryServiceResponceModel.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping("/get-document-by-text")
     public @ResponseBody
     ResponseEntity<List<ElasticQueryServiceResponceModel>> getDocumentByText(@RequestBody @Valid ElasticQueryServiceRequestModel elasticQueryServiceRequestModel){
@@ -48,4 +100,13 @@ public class ElasticDocumentController {
         response.add(elasticQueryServiceResponceModel);
         return ResponseEntity.ok(response);
     }
+
+    private ElasticQueryServiceResponceModelV2 getmodelV2(ElasticQueryServiceResponceModel model1){
+        return ElasticQueryServiceResponceModelV2.builder()
+                .id(Long.getLong(model1.getId()))
+                .userId(model1.getUserId()).text(model1.getText())
+                .text2("Version V2 text").build()
+                .add(model1.getLinks());
+    }
+
 }
--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.microservices.demo.elastic.query.service.config;
 
 import com.microservices.demo.config.UserAuthConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +21,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public WebSecurityConfig(UserAuthConfig userAuthConfig) {
         this.userAuthConfig = userAuthConfig;
     }
+
+    @Value("${security.paths-to-ignore}")
+    private String[] pathsToIgnore;
     @Override
     public void configure(HttpSecurity http) throws Exception{
         http
@@ -33,6 +38,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     }
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception{
+        webSecurity.ignoring()
+                .antMatchers(pathsToIgnore);
+    }
+
     @Override
     public  void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
